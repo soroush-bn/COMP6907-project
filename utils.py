c6b3175edf2cb78f398ee7da23c8751f1ff208cd
soroush-bn <soroush.baghernezhad3@gmail.com>
2024-11-28 15:15:14 -0330

    fix gpu usage

--- utils.py
+++ utils.py
@@ -29,7 +29,7 @@ def get_gpu_usage():
 
             gpus.append(gpu_info)
 
-        return gpus
+        return gpu_info["GPU_Utilization_Percentage"]
 
     except Exception as e:
         print(f"Error while retrieving GPU usage: {e}")
